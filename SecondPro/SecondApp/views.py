2aab287736dd1b035b86d2aca5a034f2330c65fe
okprogrammmer <omk7912@gmail.com>
2017-12-30 22:31:28 +0530

    Add all files

---
+++ SecondPro/SecondApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from SecondApp.models import User
+# Create your views here.
+def index(request):
+    return render(request,'SecondApp/index.html')
+
+def users(request):
+    user_list=User.objects.order_by('first_name')
+    user_dict={'users':user_list}
+    return render(request,'SecondApp/users.html',context=user_dict)
